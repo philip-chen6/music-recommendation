916ec39b51a56d381dc065202b6cc5c71d0f0f8c
Aaron D <76273345+aarond67@users.noreply.github.com>
2024-09-04 22:17:30 +0000

    finishing html stuff

--- app.py
+++ app.py
@@ -13,15 +13,56 @@ def render_home():
    # songs = render_template('my-form.html')
    # return redirect(url_for('get_songs'))
 
-@app.route('/input_song', methods = ["GET", "POST"])
+@app.route('/input_song', methods = ["GET","POST"])
 def render_input_songs():
-   # if request.method == "POST":
-   #    print("hellloo2")
-   #    song = request.form.get("song")
-   #    artist = request.form.get("artist") 
-   #    number = request.form.get("number")
-   #    # session[(song, artist, number)] = song
-   #    return redirect(url_for('display_songs'))
+   if request.method == "POST":
+      song = request.form["song"]
+      artist = request.form['artist']
+      number = int(request.form['number'])
+
+      
+      tracks = spotify.get_tracks(song, artist, number)
+      urls = []
+      names = []
+      ids = []
+
+      for element in tracks:
+         #print(element)
+         #print (element.keys())
+         #print(element.get('external_urls'))
+         ids.append(element.get('id'))
+         urls.append(element.get('external_urls'))
+         
+
+      #print(urls)
+      # tracks_info = [(sng['name'], sng['external_urls']['preview_url']) for sng in tracks['items']]
+      # print(tracks_info)
+      urls = [d['spotify'] for d in urls ]
+
+      with open("templates/input_song.html") as inf:
+         txt = inf.read()
+         soup = bs4.BeautifulSoup(txt, features='html.parser')
+
+      content_block = soup.find(id="content")
+      print("Found content block:", content_block)  # Debug: Check if the block is found
+      content_block.clear()
+      if content_block:
+         for i in range(len(urls)):
+            src1 = ("https://open.spotify.com/embed/track/" + ids[i])
+            new_link = soup.new_tag("iframe", style="border-radius:12px",src=src1, width ="100%", height="352", frameBorder="0", allowfullscreen="", allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture", loading="lazy")
+         
+            content_block.append(new_link)
+            content_block.append(soup.new_tag("br"))  # Adds a line break after each link
+
+            print(soup)
+         with open("templates/input_song.html", "w") as outf:
+            outf.write(str(soup))
+
+         print("Links added successfully!")
+      else:
+         print("Content block not found. No links were added.")
+   
+      
    return render_template('input_song.html')
 
 @app.route("/display_songs")
